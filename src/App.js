88c07c86cbf632ab26d98a4d958f66d9728df282
cawilson1 <cawilson1@g.cofc.edu>
2020-04-29 11:53:46 -0400

    increment by user given input amount

--- src/App.js
+++ src/App.js
@@ -4,19 +4,31 @@ import "./App.css";
 const INCREMENT_COUNTER = "INCREMENT_COUNTER";
 const DECREMENT_COUNTER = "DECREMENT_COUNTER";
 
+const UPDATE_N = "UPDATE_N";
+
+const INCREMENT_BY_N = "INCREMENT_BY_N";
+const DECREMENT_BY_N = "DECREMENT_BY_N";
+
 const reducer = (state, action) => {
   switch (action.type) {
     case INCREMENT_COUNTER:
       return { ...state, counter: state.counter + 1 };
     case DECREMENT_COUNTER:
       return { ...state, counter: state.counter - 1 };
+    case UPDATE_N:
+      return { ...state, n: action.n };
+    case INCREMENT_BY_N:
+      return { ...state, counter: state.counter + state.n };
+    case DECREMENT_BY_N:
+      return { ...state, counter: state.counter - state.n };
     default:
       return state;
   }
 };
 
 const initialState = {
-  counter: 0
+  counter: 0,
+  n: 0
 };
 
 function incrementCounter() {
@@ -24,6 +36,17 @@ function incrementCounter() {
 }
 function decrementCounter() {
   return { type: DECREMENT_COUNTER };
+}
+
+function updateN(n) {
+  return { type: UPDATE_N, n: n };
+}
+
+function incrementByN() {
+  return { type: INCREMENT_BY_N };
+}
+function decrementByN() {
+  return { type: DECREMENT_BY_N };
 }
 
 function App() {
@@ -35,6 +58,18 @@ function App() {
         <div>{state.counter}</div>
         <button onClick={() => dispatch(incrementCounter())}>Increment</button>
         <button onClick={() => dispatch(decrementCounter())}>Decrement</button>
+
+        <div>
+          <label>Number to increment by</label>
+          <input onChange={e => dispatch(updateN(Number(e.target.value)))} />
+          <br />
+          <button onClick={() => dispatch(incrementByN())}>
+            Increment by n
+          </button>
+          <button onClick={() => dispatch(decrementByN())}>
+            Decrement by n
+          </button>
+        </div>
       </header>
     </div>
   );
