94d110df4ec2f2bcdebcacf7f44895237d539345
cawilson1 <cawilson1@g.cofc.edu>
2020-04-29 11:29:47 -0400

    make string literals, convert dispatch to use action creator functions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useReducer } from "react";
 import "./App.css";
 
+const INCREMENT_COUNTER = "INCREMENT_COUNTER";
+const DECREMENT_COUNTER = "DECREMENT_COUNTER";
+
 const reducer = (state, action) => {
   switch (action.type) {
-    case "INCREMENT_COUNTER":
+    case INCREMENT_COUNTER:
       return { ...state, counter: state.counter + 1 };
-    case "DECREMENT_COUNTER":
+    case DECREMENT_COUNTER:
       return { ...state, counter: state.counter - 1 };
     default:
       return state;
@@ -16,6 +19,13 @@ const initialState = {
   counter: 0
 };
 
+function incrementCounter() {
+  return { type: INCREMENT_COUNTER };
+}
+function decrementCounter() {
+  return { type: DECREMENT_COUNTER };
+}
+
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
   console.log("state", state);
@@ -23,12 +33,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div>{state.counter}</div>
-        <button onClick={() => dispatch({ type: "INCREMENT_COUNTER" })}>
-          Increment
-        </button>
-        <button onClick={() => dispatch({ type: "DECREMENT_COUNTER" })}>
-          Decrement
-        </button>
+        <button onClick={() => dispatch(incrementCounter())}>Increment</button>
+        <button onClick={() => dispatch(decrementCounter())}>Decrement</button>
       </header>
     </div>
   );
